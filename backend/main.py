fd8a96fe74a2764ecb78e198581e0856201a8479
Micu Ana <ana.micu@oxidos.io>
2025-09-16 01:11:54 +0300

    added signup and login routes

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
 from flask_migrate import Migrate
 from config import DevConfig
 from exts import db
 from models import AIDatabase, User
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -14,6 +16,7 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 db.init_app(app)
 migrate = Migrate(app, db)
+JWTManager(app)
 
 api = Api(app, doc='/docs')
 
@@ -41,6 +44,67 @@ user_model = api.model(
         'email': fields.String(required=True),
     }
 )
+
+signup_model = api.model(
+    'SignUp',
+    {
+        'username': fields.String(required=True),
+        'email': fields.String(required=True),
+        'password': fields.String(required=True),
+    }
+)
+
+login_model = api.model(
+    'Login',
+    {
+        'username': fields.String(required=True),
+        'password': fields.String(required=True),
+    }
+)
+
+@api.route('/signup')
+class SignUp(Resource):
+    @api.expect(signup_model)
+    def post(self):
+        data = request.get_json()
+
+        username = data.get('username')
+
+        db_user = User.query.filter_by(username=username).first()
+        if db_user:
+            return jsonify({"message":f"User with username {username} already exists."})
+
+        new_user = User(
+            username = data.get('username'),
+            email = data.get('email'),
+            password = generate_password_hash(data.get('password'))  
+        )
+        new_user.save()
+
+        return jsonify({"message": "User created successfully."})
+        
+
+@api.route('/login')
+class Login(Resource):
+
+    @api.expect(login_model)
+    def post(self):
+        data = request.get_json()
+
+        username = data.get('username')
+        password = data.get('password')
+
+        db_user = User.query.filter_by(username=username).first()
+        if db_user and check_password_hash(db_user.password, password):
+
+            access_token = create_access_token(identity=db_user.username)
+            refresh_token = create_refresh_token(identity=db_user.username)
+
+            return jsonify({"access token": access_token, "refresh token": refresh_token})
+            
+        return jsonify({"message": "Invalid username or password"}), 401
+
+
 
 # endpoint pentru utilizatori
 @api.route('/users')
@@ -80,6 +144,8 @@ class DatabaseListResource(Resource):
 class DatabaseUploadResource(Resource):
 
     @api.marshal_with(db_model)
+    @api.expect(db_model)
+    @jwt_required()
     def post(self):
         """ Upload a new database file """
         file = request.files.get('file')
@@ -125,10 +191,12 @@ class DatabaseUploadResource(Resource):
 class DatabaseResource(Resource):
 
     @api.marshal_with(db_model)
+    @jwt_required()
     def get(self, id):
         """  Get data of a single database """
         return AIDatabase.query.get_or_404(id)
 
+    @jwt_required()
     def delete(self, id):
         """ Delete a database and its file """
         db_entry = AIDatabase.query.get_or_404(id)
